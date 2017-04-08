c5d2682b8fa25e5234b8452c861e609225e34c6f
marianne121 <marianne.quek@gmail.com>
2017-04-09 03:52:44 +0800

    Test case for redo add command

--- src/test/java/guitests/RedoCommandTest.java
+++ src/test/java/guitests/RedoCommandTest.java
@@ -1,5 +1,31 @@
 package guitests;
 
+import org.junit.Test;
+
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.testutil.TestTask;
+import seedu.address.testutil.TestUtil;
+
 public class RedoCommandTest extends TaskManagerGuiTest {
+
+    TestTask[] expectedList = td.getTypicalTasks();
+    TestTask[] currentList = expectedList;
+
+
+    public void redo() {
+        redoAdd();
+    }
+
+    /**
+     * Tries to redo an add command
+     */
+    @Test
+    public void redoAdd() {
+        TestTask taskToAdd = td.alice;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        commandBox.runCommand(UndoCommand.COMMAND_WORD);
+        expectedList = TestUtil.addTasksToList(expectedList, taskToAdd);
+        assertRedoSuccess(currentList, expectedList);
+    }
 
 }
