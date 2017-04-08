d9eb214e9b4531845582f4530fa84dbfdaac6be7
marianne121 <marianne.quek@gmail.com>
2017-04-09 06:49:11 +0800

    Add test case for undo delete command

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import seedu.address.logic.commands.UndoCommand;
 import seedu.address.testutil.TestTask;
-import seedu.address.testutil.TestUtil;
+import seedu.address.logic.commands.DeleteCommand;
 
 public class UndoCommandTest extends TaskManagerGuiTest {
 
@@ -19,6 +19,17 @@ public class UndoCommandTest extends TaskManagerGuiTest {
     public void undoAdd() {
         TestTask taskToAdd = td.alice;
         commandBox.runCommand(taskToAdd.getAddCommand());
+        assertUndoSuccess(expectedList);
+    }
+
+    /**
+     * Tries to undo a delete command
+     */
+    @Test
+    public void undoDelete() {
+        int targetIndex = 1;
+        commandBox.runCommand(DeleteCommand.COMMAND_WORD + " " + targetIndex);
+
         assertUndoSuccess(expectedList);
     }
 
