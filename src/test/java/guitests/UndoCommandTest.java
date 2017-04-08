c022d4e88cfc3d44666dd3dcecc440004f6db526
marianne121 <marianne.quek@gmail.com>
2017-04-09 06:35:23 +0800

    Add test case for undo add command

--- src/test/java/guitests/UndoCommandTest.java
+++ src/test/java/guitests/UndoCommandTest.java
@@ -1,5 +1,25 @@
 package guitests;
 
-public class UndoCommandTest {
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import seedu.address.logic.commands.UndoCommand;
+import seedu.address.testutil.TestTask;
+import seedu.address.testutil.TestUtil;
+
+public class UndoCommandTest extends TaskManagerGuiTest {
+
+    private TestTask[] expectedList = td.getTypicalTasks();
+
+    /**
+     * Tries to undo an add command
+     */
+    @Test
+        TestTask taskToAdd = td.alice;
+        commandBox.runCommand(taskToAdd.getAddCommand());
+        assertUndoSuccess(expectedList);
+    }
 
 }
+
