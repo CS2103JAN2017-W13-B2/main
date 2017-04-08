a71229263668fa32815408d79f2ee155d474bee7
marianne121 <marianne.quek@gmail.com>
2017-04-09 04:44:00 +0800

    Add test case for redo edit command

--- src/test/java/guitests/RedoCommandTest.java
+++ src/test/java/guitests/RedoCommandTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.DeleteCommand;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.MarkCommand;
 import seedu.address.logic.commands.RedoCommand;
 import seedu.address.logic.commands.UndoCommand;
+import seedu.address.model.task.Name;
 import seedu.address.testutil.TestTask;
 import seedu.address.testutil.TestUtil;
 
@@ -24,6 +27,7 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         TestTask taskToAdd = td.alice;
         commandBox.runCommand(taskToAdd.getAddCommand());
         commandBox.runCommand(UndoCommand.COMMAND_WORD);
+
         expectedList = TestUtil.addTasksToList(expectedList, taskToAdd);
         assertRedoSuccess(expectedList);
     }
@@ -36,6 +40,7 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         int targetIndex = 1;
         commandBox.runCommand(DeleteCommand.COMMAND_WORD + " " + targetIndex);
         commandBox.runCommand(UndoCommand.COMMAND_WORD);
+
         expectedList = TestUtil.removeTaskFromList(expectedList, targetIndex);
         assertRedoSuccess(expectedList);
     }
@@ -65,6 +70,21 @@ public class RedoCommandTest extends TaskManagerGuiTest {
         commandBox.runCommand(UndoCommand.COMMAND_WORD);
 
         expectedList[targetIndex - 1].setStatus("Done");
+        assertRedoSuccess(expectedList);
+    }
+
+    /**
+     * Tries to redo an edit command
+     * @throws IllegalValueException
+     */
+    @Test
+    public void redoEdit() throws IllegalValueException {
+        int targetIndex = 1;
+        String detailsToEdit = "Bobby";
+        commandBox.runCommand(EditCommand.COMMAND_WORD + " " + 1 + " " + detailsToEdit);
+        commandBox.runCommand(UndoCommand.COMMAND_WORD);
+
+        expectedList[targetIndex - 1].setName(new Name("Bobby"));
         assertRedoSuccess(expectedList);
     }
 
