5aebce7e2069f3887102cd637aa38a8b834355c7
marianne121 <marianne.quek@gmail.com>
2017-04-10 02:01:26 +0800

    Add test case for invalid command for find

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -1,11 +1,13 @@
 package guitests;
 
 import static org.junit.Assert.assertTrue;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.EditCommand;
+import seedu.address.logic.commands.FindCommand;
 import seedu.address.testutil.TestTask;
 
 public class FindCommandTest extends TaskManagerGuiTest {
@@ -13,44 +15,53 @@ public class FindCommandTest extends TaskManagerGuiTest {
   //@@author A0143504R
     @Test
     public void findByKeywords() {
-        assertFindResult("find Meier", td.benson, td.daniel); // find by name, multiple results
-        assertFindResult("find friends", td.alice, td.benson); // find by tags, multiple results
-        assertFindResult("find find", td.alice, td.benson, td.fiona); // find by description, multiple results
+        assertFindResult(FindCommand.COMMAND_WORD + " Meier", td.benson, td.daniel); // find by name, multiple results
+        assertFindResult(FindCommand.COMMAND_WORD + " friends", td.alice, td.benson); // find by tags, multiple results
+        // find by description, multiple results
+        assertFindResult(FindCommand.COMMAND_WORD + " find", td.alice, td.benson, td.fiona);
 
       // find after adding one task
         commandBox.runCommand(td.benson.getAddCommand());
-        assertFindResult("find Meier", td.benson, td.daniel, td.benson);
+        assertFindResult(FindCommand.COMMAND_WORD + " Meier", td.benson, td.daniel, td.benson);
 
       //find after deleting one result
         commandBox.runCommand("delete 3");
-        assertFindResult("find Meier", td.benson, td.daniel);
+        assertFindResult(FindCommand.COMMAND_WORD + " Meier", td.benson, td.daniel);
 
         //find after editing one task
         int targetIndex = 2;
         String detailsToEdit = "Bobby";
         commandBox.runCommand(EditCommand.COMMAND_WORD + " " + targetIndex + " " + detailsToEdit);
-        assertFindResult("find Meier", td.daniel);
+        assertFindResult(FindCommand.COMMAND_WORD + " Meier", td.daniel);
     }
 
     @Test
     public void findByDeadline() {
-        assertFindResult("find dl/10/10/2010", td.daniel);
-        assertFindResult("find dl/11-11-2011", td.carl);
-        assertFindResult("find dl/27 feb 2025", td.benson);
+        assertFindResult(FindCommand.COMMAND_WORD + " dl/10/10/2010", td.daniel);
+        assertFindResult(FindCommand.COMMAND_WORD + " dl/11-11-2011", td.carl);
+        assertFindResult(FindCommand.COMMAND_WORD + " dl/27 feb 2025", td.benson);
     }
 
   //@@author
     @Test
     public void find_emptyList() {
         commandBox.runCommand("clear");
-        assertFindResult("find Jean"); // no results
+        assertFindResult(FindCommand.COMMAND_WORD + " Jean"); // no results
     }
 
     @Test
-    public void find_invalidCommand_fail() {
-        commandBox.runCommand("findgeorge");
+    public void find_unknownCommand_fail() {
+        commandBox.runCommand(FindCommand.COMMAND_WORD + "george");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
+
+    //@@author A0143504R
+    @Test
+    public void find_invalidCommand_fail() {
+        commandBox.runCommand(FindCommand.COMMAND_WORD + " Meier dl/10/10/2010");
+        assertResultMessage(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+    }
+    //@@author
 
     private void assertFindResult(String command, TestTask... expectedHits) {
         commandBox.runCommand(command);
