82e96de1d4929d3f6266f32ab9e3452f6b13abed
marianne121 <marianne.quek@gmail.com>
2017-04-09 16:47:46 +0800

    Add find by keywords test cases

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import seedu.address.commons.core.Messages;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.testutil.TestTask;
 
 public class FindCommandTest extends TaskManagerGuiTest {
@@ -16,9 +17,27 @@ public class FindCommandTest extends TaskManagerGuiTest {
 
         //find after deleting one result
         commandBox.runCommand("delete 1");
+    public void find_byKeywords() {
+        assertFindResult("find Meier", td.benson, td.daniel); // find by name, multiple results
+        assertFindResult("find friends", td.alice, td.benson); // find by tags, multiple results
+        assertFindResult("find find", td.alice, td.benson, td.fiona); // find by description, multiple results
+
+      // find after adding one task
+        commandBox.runCommand(td.benson.getAddCommand());
+        assertFindResult("find Meier", td.benson, td.daniel, td.benson);
+
+      //find after deleting one result
+        commandBox.runCommand("delete 3");
+        assertFindResult("find Meier", td.benson, td.daniel);
+
+        //find after editing one task
+        int targetIndex = 2;
+        String detailsToEdit = "Bobby";
+        commandBox.runCommand(EditCommand.COMMAND_WORD + " " + targetIndex + " " + detailsToEdit);
         assertFindResult("find Meier", td.daniel);
     }
 
+    @Test
     @Test
     public void find_emptyList() {
         commandBox.runCommand("clear");
